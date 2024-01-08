76a60fa17646375cff10492214e9a98cfee05bd3
inshining <inshining.kim@gmail.com>
2024-01-08 19:19:56 +0900

    🍀[FEAT] : openai 연결

--- functions/main.py
+++ functions/main.py
@@ -2,17 +2,35 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
+from openai import OpenAI
+
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+import os
 
+initialize_app()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 @https_fn.on_request()
 def my_chatbot(req: https_fn.Request) -> https_fn.Response:
     try:
         user_query = req.data.decode('utf-8')
         
-        res = "response : "  + user_query
-        
-        return https_fn.Response(res)
+        response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_query 
+            }
+        ],
+        model="gpt-3.5-turbo"
+        )
+
+        generated_text = response.choices[0].text
+
+        return https_fn.Response(generated_text)
 
     except Exception as e:
-        return https_fn.REsponse(str(e))
+        return https_fn.Response(str(e))
